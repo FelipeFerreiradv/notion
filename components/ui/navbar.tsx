df8976e11fa5d336e2ff948c9b9b971349bb569b
Felipe Ferreira <fefelbf@gmail.com>
2025-01-09 16:59:01 -0300

    feat: add a lint commitlint and husky

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronDown, ChevronRight, Menu, X } from "lucide-react";
+import { ChevronDown, ChevronRight, ChevronUp, Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./button";
@@ -8,9 +8,19 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpenArrow, setIsOpenArrow] = useState<boolean>(false);
+  const [isOpenResourses, setIsOpenResourses] = useState<boolean>(false);
 
   const toggleMenu = () => {
     setIsOpen((prevState) => !prevState);
+  };
+
+  const toggleMenuArrow = () => {
+    setIsOpenArrow((prevState) => !prevState);
+  };
+
+  const toggleMenuResources = () => {
+    setIsOpenResourses((prevState) => !prevState);
   };
 
   return (
@@ -38,23 +48,88 @@ const Navbar = () => {
                 </Link>
                 <ChevronDown width={14} height={14} />
               </div>
-              <div className="flex items-center gap-2">
-                <Link href="#" className="font-semibold text-[15px]">
-                  Download
-                </Link>
-                <ChevronDown width={14} height={14} />
+              <div
+                className="relative flex flex-col"
+                onMouseEnter={toggleMenuArrow}
+              >
+                <div className="flex items-center gap-2">
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Download
+                  </Link>
+                  {isOpenArrow ? (
+                    <ChevronUp
+                      width={14}
+                      height={14}
+                      className="cursor-pointer"
+                    />
+                  ) : (
+                    <ChevronDown
+                      width={14}
+                      height={14}
+                      className="cursor-pointer"
+                    />
+                  )}
+                </div>
+                <div
+                  className={
+                    !isOpenArrow
+                      ? "hidden"
+                      : "absolute left-0 top-12 flex flex-col gap-3"
+                  }
+                >
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Notion
+                  </Link>
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Calendar
+                  </Link>
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Web Clipper
+                  </Link>
+                </div>
               </div>
-              <div className="flex items-center gap-2">
-                <Link href="#" className="font-semibold text-[15px]">
-                  Solutions
-                </Link>
-                <ChevronDown width={14} height={14} />
-              </div>
-              <div className="flex items-center gap-2">
-                <Link href="#" className="font-semibold text-[15px]">
-                  Resources
-                </Link>
-                <ChevronDown width={14} height={14} />
+              <div
+                className="relative flex flex-col z-50"
+                onMouseEnter={toggleMenuResources}
+              >
+                <div className="flex items-center gap-2">
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Resources
+                  </Link>
+                  {!isOpenResourses ? (
+                    <ChevronUp
+                      width={14}
+                      height={14}
+                      className="cursor-pointer"
+                    />
+                  ) : (
+                    <ChevronDown
+                      width={14}
+                      height={14}
+                      className="cursor-pointer"
+                    />
+                  )}
+                </div>
+                <div
+                  className={
+                    isOpenResourses
+                      ? "hidden"
+                      : "absolute left-0 top-12 flex flex-col gap-3"
+                  }
+                >
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Personal
+                  </Link>
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Students
+                  </Link>
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Teachers
+                  </Link>
+                  <Link href="#" className="font-semibold text-[15px]">
+                    Creators
+                  </Link>
+                </div>
               </div>
               <Link href="#" className="font-semibold text-[15px]">
                 Pricing
@@ -76,7 +151,11 @@ const Navbar = () => {
           </div>
         </section>
         <div className="hidden max-lg:block" onClick={toggleMenu}>
-          {isOpen ? <X /> : <Menu className="cursor-pointer" />}
+          {isOpen ? (
+            <X className="cursor-pointer" />
+          ) : (
+            <Menu className="cursor-pointer" />
+          )}
         </div>
       </header>
       <header
@@ -87,30 +166,33 @@ const Navbar = () => {
         }
       >
         <section className="flex flex-col gap-8 w-[100% - 12px] my-6 mx-4">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between cursor-pointer">
             <p className="font-bold text-lg">Product</p>
             <ChevronRight />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between cursor-pointer">
             <p className="font-bold text-lg">Teams</p>
             <ChevronRight />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between cursor-pointer">
             <p className="font-bold text-lg">Individuals</p>
             <ChevronRight />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between cursor-pointer">
             <p className="font-bold text-lg">Download</p>
             <ChevronRight />
           </div>
-          <p className="font-bold text-lg">Pricing</p>
-          <p className="font-bold text-lg">Request a demo</p>
+          <p className="font-bold text-lg cursor-pointer">Pricing</p>
+          <p className="font-bold text-lg cursor-pointer">Request a demo</p>
         </section>
         <div className="flex flex-col items-center justify-center gap-4 mb-24">
           <Button className="w-[96%] h-[36px] font-bold text-lg text-white bg-[#1a1a19]">
             Get Notion free
           </Button>
-          <Button className="w-[96%] h-[36px] text-lg text-black bg-transparent">
+          <Button
+            variant="ghost"
+            className="w-[96%] h-[36px] text-lg text-black bg-transparent"
+          >
             Log in
           </Button>
         </div>
